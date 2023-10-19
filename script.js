962a16acaa51de3ddc18810fe142113d21163c95
ANISH KUMAR <70019437+Amish2417@users.noreply.github.com>
2023-10-19 16:08:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+for(let i=1;i<=800;i++){
+    const container = document.querySelector('.container');
+    const dupdiv = document.createElement("div");
+    dupdiv.classList.add("square");
+    container.appendChild(dupdiv);
+
+}
